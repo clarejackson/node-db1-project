b34ce488350084554fc1d494775b4c1565c3d088
Clare Jackson <clare-jackson@lambdastudents.com>
2021-02-25 17:35:34 -0500

    router complete, testing now'

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -32,19 +32,21 @@ router.post('/', checkAccountNameUnique,  checkAccountPayload(), (req, res, next
   }
 })
 
-router.put('/:id', checkAccountId, (req, res, next) => {
+router.put('/:id', checkAccountPayload(), checkAccountId, checkAccountNameUnique, (req, res, next) => {
   // DO YOUR MAGIC
   try {
-
+    const updatedAccount = accounts.updateById(req.params.id, req.body)
+    res.status(200).json(updatedAccount)
   } catch (err) {
     next (err)
   }
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
   try {
-
+    accounts.deleteById(req.params.id)
+    res.status(204).end()
   } catch (err) {
     next (err)
   }
