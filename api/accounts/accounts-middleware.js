fe874164a392d942bcecebdc5496c8f9836e7e0c
Clare Jackson <clare-jackson@lambdastudents.com>
2021-02-24 23:24:24 -0500

    first middleware done

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,5 +1,33 @@
-exports.checkAccountPayload = (req, res, next) => {
+exports.checkAccountPayload = () => {
   // DO YOUR MAGIC
+  return (req, res, next) => {
+    if (!req.body.name || !req.body.budget) {
+      return res.status(400).json({
+        message: "name and budget are required"
+      })
+    }
+    if (typeof req.body.name !== "string") {
+      return res.status(400).json({
+        message: "name of account must be a string"
+      })
+    }
+    if (100 < ((req.body.name).trim) < 3) {
+      return res.status(400).json({
+        message: "name of account must be between 3 and 100"
+      })
+    }
+    if (isNaN(req.body.budget)) {
+      return res.status(400).json({
+        message: "budget of account must be a number"
+      })
+    }
+    if (1000000 < (req.body.budget) < 0) {
+      return res.status(400).json({
+        message: "budget of account is too large or too small"
+      })
+    }
+    next()
+  }
 }
 
 exports.checkAccountNameUnique = async (req, res, next) => {
