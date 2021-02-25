5ebf31a125e9e1ce6a09945d93053aed04c71efe
Clare Jackson <clare-jackson@lambdastudents.com>
2021-02-24 19:59:29 -0500

    server working and some of the model done

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,13 +1,21 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
   // DO YOUR MAGIC
+  return db('accounts');
 }
 
 const getById = id => {
   // DO YOUR MAGIC
+  return db('accounts')
+  .where({ id })
+  .first()
 }
 
 const create = async account => {
   // DO YOUR MAGIC
+  return db('accounts')
+  .insert(account)
 }
 
 const updateById = async (id, account) => {
--- api/server.js
+++ api/server.js
@@ -1,7 +1,23 @@
 const express = require("express");
 
+// const accountsRouter = require("./accounts/accounts-router")
+
 const server = express();
 
 server.use(express.json());
+
+// server.use("/api/accounts", accountsRouter)
+
+server.get('/', (req, res) => {
+  res.status(200).json({api: "Hello World!"});
+});
+
+server.use((err, req, res, next) => {
+  console.log(err)
+
+  res.status(500).json({
+    message: "Something went wrong, please try again"
+  })
+})
 
 module.exports = server;
